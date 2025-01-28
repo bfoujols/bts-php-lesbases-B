593bcf6bdb37e544c64e9461da8307f9ae68c5fd
Benoit Foujols <benoit@foujols.com>
2025-01-28 11:44:57 +0100

    Update condition.php

---
+++ syntaxe/condition.php
@@ -0,0 +1,35 @@
+<?php
+    $name = "benoit";
+
+    /**
+     * - "==" (égalité)
+     * - "!=" (inégalité)
+     * - ">" (plus grand)
+     * - ">=" (plus grand ou égal)
+     * - "<" (plus petit)
+     * - "<=" (plus petit ou égal)
+     * - "&&" (ET logique)
+     * - "||" (OU logique)
+     * - "!" (NON logique)
+     */
+
+    if ($name == "benoit") {
+        echo "Je m'appelle " . $name . " \n";
+    } elseif ($name === 1)
+        echo "Le chiffre est " . $name . " \n";
+    else {
+        echo "pas de prénom \n";
+    }
+
+    switch ($name) {
+        case 1:
+            echo "Le chiffre est " . $name . " \n";
+            break;
+        case "benoit":
+            echo "Je m'appelle " . $name . " \n";
+            break;
+        default:
+            echo "pas de prénom \n";
+    }
+
+?>
